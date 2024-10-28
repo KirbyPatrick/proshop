79270539f81651bfd466179d58dd004dfe17e748
“Patrick <pkirbyinatx@gmail.com>
2024-10-28 10:16:23 -0500

    installed react bootstrap and built out nav

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import { fashoppingcart, FaUser } from 'react-icons/fa'
+
+const Header = () => {
+  return (
+    <header>
+        <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect> 
+            <Container>
+                <Navbar.Brand href="/">ProShop</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link href="/cart"><fashoppingcart /> Cart</Nav.Link>
+                        <Nav.Link href="/login"><FaUser /> Sign In</Nav.Link>
+                    
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        
+        </Navbar>
+    </header>
+  )
+}
+
+export default Header
